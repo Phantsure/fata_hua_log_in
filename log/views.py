d185363999d5f3ae19e54424fcf6b7b703091283
Phantsure <samparksharma2000@gmail.com>
2019-05-23 18:25:45 +0530

    functionality vulnerable to xxe made

--- log/views.py
+++ log/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import request, HttpResponse
+from lxml import etree
 
 from .forms import StudentForm
 from .functions import handle_uploaded_file
@@ -14,7 +15,10 @@ def log(request):
         student = StudentForm(request.POST, request.FILES)
         if student.is_valid():
             handle_uploaded_file(request.FILES['file'])
-            return HttpResponse("Upload successful")
+            parser = etree.XMLParser(load_dtd=True, resolve_entities=True)
+            tree = etree.parse('log/upload/xe.xml', parser=parser)
+            root = tree.getroot()
+            return HttpResponse(root.text)
     else:
         student = StudentForm()
         return render(request, 'log/login.html', {'form':student})  
