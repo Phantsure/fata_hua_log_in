ce5952d2f82edd8699e2b9c626bce1674a77845b
Phantsure <samparksharma2000@gmail.com>
2019-05-24 00:22:22 +0530

    update2

--- log/views.py
+++ log/views.py
@@ -14,10 +14,13 @@ def log(request):
     if request.method == 'POST':
         student = StudentForm(request.POST, request.FILES)
         if student.is_valid():
-            handle_uploaded_file(request.FILES['file'])
+            f = request.FILES['file']
+            handle_uploaded_file(f)
             parser = etree.XMLParser(load_dtd=True, resolve_entities=True)
-            tree = etree.parse('log/upload/xe.xml', parser=parser)
+            tree = etree.parse('log/upload/' + f.name, parser=parser)
+            # extraction of information
             root = tree.getroot()
+            
             return HttpResponse(root.text)
     else:
         student = StudentForm()
